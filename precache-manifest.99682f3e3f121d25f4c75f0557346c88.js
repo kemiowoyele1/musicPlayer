493603bda70fda2956575911b08625e22cf7f3f5
tiidelab <info@techspecialistlimited.com>
2020-03-17 10:00:56 +0100

    first commit

---
+++ precache-manifest.99682f3e3f121d25f4c75f0557346c88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75db702de2df9253195",
+    "url": "/css/app.e55dd9f6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "df7813fe809c6515bf6319aa624bdbb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce414aed218ebf4bec4",
+    "url": "/js/about.6c088e08.js"
+  },
+  {
+    "revision": "b75db702de2df9253195",
+    "url": "/js/app.1ca68e82.js"
+  },
+  {
+    "revision": "eca94e013a19b65867e7",
+    "url": "/js/chunk-vendors.8d57be69.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "944ae21e6c9f19adc9fcee07e624e5c6",
+    "url": "/media/GOD-bless-the-USA.944ae21e.mp3"
+  },
+  {
+    "revision": "2a51cff5402ac824ae218171c821bdb2",
+    "url": "/media/glory-to-GOD.2a51cff5.mp3"
+  },
+  {
+    "revision": "08d6c061b4f6e24d714c739958c9cec8",
+    "url": "/media/hes-alive.08d6c061.mp3"
+  },
+  {
+    "revision": "5cc8692617618055c0d3937093224d4e",
+    "url": "/media/silver-and-gold.5cc86926.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
